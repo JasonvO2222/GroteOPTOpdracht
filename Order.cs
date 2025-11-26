600f0faf000ee49273fa0f60e4c48d9c97ab9869
JJ <ja.van.otterlo@gmail.com>
2025-11-26 16:22:35 +0100

    Parsing txt files

---
+++ Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroteOPTOpdracht
+{
+    internal class Order
+    {
+        public int orderId {  get; set; }
+        public string place;
+        public int frequency;
+        public int containerCount;
+        public int containerVolume;
+        public float loadingTime; //in minutes
+        public int matrixId;
+        public int XCoordinate;
+        public int YCoordinate;
+
+        public Order(string line) {
+            string[] results = line.Split(';');
+            this.orderId = int.Parse(results[0]);
+            this.place = results[1];
+            this.frequency = int.Parse(results[2]);
+            this.containerCount = int.Parse(results[3]);
+            this.containerVolume = int.Parse(results[4]);
+            this.loadingTime = float.Parse(results[5]);
+            this.matrixId = int.Parse(results[6]);
+            this.XCoordinate = int.Parse(results[7]);
+            this.YCoordinate = int.Parse(results[8]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace GroteOPTOpdracht
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            //initialize datastructures
+            int[,,] afstandenMatrix = new int[1099, 1099, 2];
+            List<Order> orderList = new List<Order>();
+
+            // parse the text files
+            StreamReader afstanden = new StreamReader("AfstandenMatrix.txt");
+            string line = afstanden.ReadLine();
+
+            while (line != null) {
+                string[] splt = line.Split(';');
+                afstandenMatrix[int.Parse(splt[0]), int.Parse(splt[1]), 0] = int.Parse(splt[2]);
+                afstandenMatrix[int.Parse(splt[0]), int.Parse(splt[1]), 0] = int.Parse(splt[3]);
+                line = afstanden.ReadLine();
+            }
+
+            StreamReader orders = new StreamReader("Orderbestand.txt");
+            line = orders.ReadLine();
+
+            while (line != null) {
+                Order ord = new Order(line);
+                orderList.Add(ord);
+            }
+
+
+
+        }
+    }
+}
